ce4e4ca2dee8e470ef81663268aaa8ae3c1e8ea6
xuyadong <xuyadong@qiyi.com>
2018-04-13 21:11:02 +0800

    修复生产组件时路径bug

--- generators/component/index.js
+++ generators/component/index.js
@@ -1,21 +1,28 @@
-var Generator = require('yeoman-generator');
+const path = require('path');
+const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.argument('cname', {
+    this.argument('cpath', {
       type: String,
       required: true,
     });
   }
   writing() {
-    const cname = this.options.cname;
+    const cpath = path.resolve('.', this.options.cpath);
+    const cname = cpath.split(/(\\|\/)/).pop();
+    // 检查文件是否已存在
+    if (this.fs.exists(path.resolve(cpath, 'index.js'))) {
+      this.log(`component ${cname} already exsits!!`);
+      return;
+    }
     this.fs.copyTpl(
       this.templatePath('component.vue'),
-      this.destinationPath(`${cname}/${cname}.vue`),
+      this.destinationPath(`${cpath}/${cname}.vue`),
       { cname }
     );
-    this.fs.write(`${cname}/index.js`, `export { default } from './${this.options.cname}'`);
-    this.fs.write(`${cname}/README.md`, `#${cname}`);
+    this.fs.write(`${cpath}/index.js`, `export { default } from './${cname}'`);
+    this.fs.write(`${cpath}/README.md`, `# ${cname}`);
   }
 };
